17efc5f146601615a5a4a86df0e43f45eacb66fe
Hamze Dokoohaki <para2x@gmail.com>
2022-02-08 13:17:16 -0600

    Update test_apsim_TL.sh

--- Champaign/test_apsim_TL.sh
+++ Champaign/test_apsim_TL.sh
@@ -1,10 +1,10 @@
 LAT="0025"
 LON="0046"
-DIRNAME="run_${LAT}_${LON}"
+DIRNAME="run_${LAT}_${LON}_$now"
 echo $DIRNAME
 rm -r "/pysims/data/${DIRNAME}"
 mkdir "/pysims/data/${DIRNAME}"
 cd "/pysims/data/${DIRNAME}"
-/pysims/data/pysims/pysims.py --param ../params.apsim.sample --campaign ../campaign_apsim --tlatidx $LAT --tlonidx $LON
+#/pysims/data/pysims/pysims.py --param ../params.apsim.sample --campaign ../campaign_apsim --tlatidx $LAT --tlonidx $LON
 
 cd ..
