fc6b5a18a4e224329f0ea99be88b2312238738e6
Hamze Dokoohaki <para2x@gmail.com>
2022-02-10 11:12:19 -0600

    Update test_apsim_TL.sh

--- Champaign/test_apsim_TL.sh
+++ Champaign/test_apsim_TL.sh
@@ -2,15 +2,23 @@
 LAT=$1
 LON=$2
 
-echo "The first fruit is: $1"
-echo "The second fruit is: $2"
+echo "The LAT is: $1"
+echo "The LON is: $2"
 
-Date=$(date +"%Y_%m_%d_%H%M")
-DIRNAME="Results/run_${LAT}_${LON}_$Date"
-echo $DIRNAME
+Date=$(date +"%Y_%m_%d")
+DIRNAME="Results/$Date"
 
-mkdir "/pysims/data/${DIRNAME}"
-cd "/pysims/data/${DIRNAME}"
+if [ ! -d $DIRNAME ]; then
+  mkdir -p $DIRNAME;
+fi
+
+
+Date2=$(date +"%Y_%m_%d_%H%M")
+DIRNAME2="$DIRNAME/run_${LAT}_${LON}_$Date"
+echo $DIRNAME2
+
+mkdir "/pysims/data/${DIRNAME2}"
+cd "/pysims/data/${DIRNAME2}"
 python /pysims/data/pysims/pysims.py --param /pysims/data/Sims/Champaign/params.apsim.sample --campaign /pysims/data/Sims/Champaign/campaign_apsim --tlatidx $LAT --tlonidx $LON
 
 cd ..
